f4740278644686ff6fe894e7ed64db18f1bc626b
35mohitgupta] <35mohitgupta@gmail.com>
2020-07-20 11:15:08 +0530

    Added kafka-connect,producer and consumer

---
+++ Kafka-Demo/src/main/java/org/mht/kafka/consumer/IdempotentConsumerDemo.java
@@ -0,0 +1,134 @@
+package org.mht.kafka.consumer;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Demo for idempotent consumer.
+ * here we put each message of the topic in a map exactly once. 
+ * @author mohit
+ *
+ */
+public class IdempotentConsumerDemo {
+	
+	private static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+	private static final String topic = "demo_topic";
+	private static final String groupName = "idempotent-java-app";
+	private static Logger logger = LoggerFactory.getLogger(IdempotentConsumerDemo.class);
+	private static Map<String, String> recordMap;
+	
+	private static final String fileName = "kafka_records"; 
+	
+	public static void main(String[] args) throws IOException {
+		
+		//creating producer config
+		logger.info("creating consumer properties");
+		Properties properties = new Properties();
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupName);
+		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // can take following value - earliest, latest or none(gives err)
+		//limiting the no of records fetched after polling request
+		properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "25");
+		
+		
+		//creating consumer
+		logger.info("creating consumer");
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+		
+		File file = getFile();
+		
+		//initialize map
+		initializeMap(file);
+		
+		
+		//subscribing topic
+		logger.info("subscribing to topic");
+		consumer.subscribe(Collections.singleton(topic));
+		// or
+//		consumer.subscribe(Arrays.asList(topic));
+		
+		//polling the data
+		logger.info("polling data starts");
+		while(true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+			logger.info("No of records fetched during poll "+records.count());
+			for(ConsumerRecord<String, String> record : records) {
+				
+				String mapKey = record.topic()+"_"+ record.partition()+"_"+ record.offset();
+				
+				logger.info("RECORD: Key: "+record.key() + " Value: "+record.value()+"\n"+
+						"Topic: "+record.topic()+" \nPartition: "+record.partition()+
+						" Offset: "+record.offset());
+				if(recordMap.containsKey(mapKey)) {
+					logger.info("Message is already processed "+record.value());
+				}else {
+					recordMap.put(mapKey, record.value());
+					saveRecord(mapKey, record.value(), file);
+				}
+			}
+		}
+		
+//		consumer.close();
+	}
+
+
+	private static File getFile() throws IOException {
+		File file =  new File(fileName);
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		return file;
+	}
+
+
+	private static void initializeMap(File file) throws FileNotFoundException {
+		recordMap = new HashMap<String, String>();
+		if(!file.exists())
+			return;
+		Scanner reader = new Scanner(file);
+		while(reader.hasNext()) {
+			String nextline = reader.nextLine();
+			logger.info("nextLine: ",nextline);
+			String[] keyVal = nextline.split(",");
+			recordMap.put(keyVal[0], keyVal[1]);
+		}	
+		reader.close();
+	}
+	
+	
+	private static void saveRecord(String key, String message, File file) throws IOException {
+		FileWriter fileWriter = new FileWriter(file,true);
+		String data = key+","+message+"\n";
+		fileWriter.write(data);
+		fileWriter.close();
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Kafka-Demo/src/main/java/org/mht/kafka/producer/SafeProducerDemo.java
@@ -0,0 +1,68 @@
+package org.mht.kafka.producer;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SafeProducerDemo {
+	
+	private static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+	private static String topic = "demo_topic";
+	private static Logger logger = LoggerFactory.getLogger(SafeProducerDemo.class);
+	
+	public static void main(String[] args) {
+		
+		//Create Producer properties
+		logger.info("creating kafka properties");
+		Properties properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		
+		// create Safe Producer
+		properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+		// adding expilitly for confirmation
+		properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+		properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+		properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); // kafka 2.X >= 1.1 so we can keep this as 5. Otherwise use 1.
+		
+		
+		logger.info("creating kafka consumer");
+		//Create Kafka Producer
+		final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+		
+		for(int i=11; i<22;i++) {
+			
+			logger.info("creating a producer record");
+			//Create Producer Record
+			ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, "record "+Integer.toString(i));
+			
+			//Send data - asynchronous
+			producer.send(record, new Callback() {
+				// This executes every time the record is sent or there is an exception
+				public void onCompletion(RecordMetadata metadata, Exception exception) {
+					if(exception == null) {
+						logger.info("Received new METADATA: \n"
+								+ "Topic: "+metadata.topic()+"\n"
+								+"Partition: "+metadata.partition()+"\n"
+								+"Offset: "+metadata.offset()+"\n"
+								+"Timestamp: "+metadata.timestamp());
+					}else {
+						logger.error("Exception occured while sending record : "+exception);
+					}
+				}
+			});
+		}
+		
+		//Closing the producer
+		producer.close();
+	}
+	
+}
